746a7c5907f4096251be172c05f25688babbbbcc
94tony <dagatatony94@gmail.com>
2021-05-30 23:08:06 +0200

    biglietto-treno

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var km = prompt("Numero Di Chilometri");
+var Age = prompt("Età del passaggero");
+
+var costo  = 0.21 * km ;
+var sconto = costo / 100 * 20;
+var risultatoMinore = costo - sconto 
+
+var costo  = 0.21 * km ;
+var scontoSenior = costo / 100 * 40;
+var risultatoSenior = costo - scontoSenior 
+
+
+if(Age < 18 ) {
+    document.getElementById("costo").innerHTML = risultatoMinore + " €";
+
+}
+
+else{ if(Age >= 65 ) {
+    document.getElementById("costo").innerHTML = risultatoSenior + " €";
+    }
+}
+
+
+
